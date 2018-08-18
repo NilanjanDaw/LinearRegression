9315458ae0aea45413515349fa90a8fb6ecda190
nilanjandaw <nilanjandaw@gmail.com>
2018-08-19 02:32:31 +0530

    basis and norms implemented and parameterised

--- lr.py
+++ lr.py
@@ -3,7 +3,7 @@
 # @Email:  nilanjandaw@gmail.com
 # @Filename: lr.py
 # @Last modified by:   nilanjan
-# @Last modified time: 2018-08-18T20:06:29+05:30
+# @Last modified time: 2018-08-19T02:31:07+05:30
 # @copyright: Nilanjan Daw
 
 
@@ -14,10 +14,11 @@ import numpy as np
 import pandas as pd
 import csv
 import math
+import argparse
 
 iterations = 500000
-learning_rate = 0.0003
-lambda_regularizer = 80000
+learning_rate = 0.0001
+lambda_regularizer = 4500
 
 error_rate = 0.00000001
 
@@ -109,22 +110,14 @@ def plot(data):
     plt.show()
 
 
-# def one_hot_encode():
-#     day = {"monday": 0, "tuesday": 1, "wednesday": 2,
-#            "thursday": 3, "friday": 4, "saturday": 5, "sunday": 6}
-#
-
-
-def read_data(file_path):
+def read_data(file_path, basis=None):
     if os.path.isfile(file_path):
         data = pd.read_csv(file_path, skiprows=[0], names=["page_likes", "page_checkin", "daily_crowd",
                                                            "page_category", "F1", "F2", "F3", "F4", "F5", "F6",
                                                            "F7", "F8", "c1", "c2", "c3", "c4", "c5", "base_time",
                                                            "post_length", "share_count", "promotion",
                                                            "h_target", "post_day", "basetime_day", "target"])
-        # data["basetime_day"] = list(map(day_to_num, data["basetime_day"]))
-        # data["post_day"] = list(map(day_to_num, data["post_day"]))
-        data.sample(frac=1).reset_index(drop=True)
+
         y_train = data.loc[:, "target"]
         basetime_day = pd.get_dummies(data["basetime_day"], prefix='basetime_day')
         post_day = pd.get_dummies(data["post_day"], prefix="post_day")
@@ -139,6 +132,13 @@ def read_data(file_path):
         data.drop(["target"], axis=1, inplace=True)
         y_train = y_train.values
         x_train = data.values
+        if basis == 'sigmoid':
+            x_train = 1 / (1 + np.exp(x_train))
+        if basis == 'normal':
+            variance = x_train.std() ** 2
+            mean = x_train.mean()
+            x_train = -((x_train - mean) ** 2) / (2 * variance)
+            x_train = 1 / math.sqrt(2 * math.pi * variance) * np.exp(x_train)
         number_of_samples, _ = np.shape(x_train)
         bias = np.ones((number_of_samples, 1))
         x_train = np.hstack((bias, x_train))
@@ -148,7 +148,7 @@ def read_data(file_path):
         print("file not found")
 
 
-def read_data_test(file_path):
+def read_data_test(file_path, basis=None):
     if os.path.isfile(file_path):
         data = pd.read_csv(file_path, skiprows=[0], names=["page_likes", "page_checkin", "daily_crowd",
                                                            "page_category", "F1", "F2", "F3", "F4", "F5", "F6",
@@ -166,8 +166,16 @@ def read_data_test(file_path):
         data.drop(["post_day"], axis=1, inplace=True)
         x_test = normalisation(data)
 
-        x_test[np.isnan(x_test)] = 0
+        x_test[np.isnan(x_test)] = 0.0
+        x_test = x_test.values
         number_of_tests, _ = np.shape(x_test)
+        if basis == "sigmoid":
+            x_test = 1 / (1 + np.exp(x_test))
+        elif basis == 'normal':
+            variance = x_test.std() ** 2
+            mean = x_test.mean()
+            x_test = -((x_test - mean) ** 2) / (2 * variance)
+            x_test = 1 / math.sqrt(2 * math.pi * variance) * np.exp(x_test)
         bias = np.ones((number_of_tests, 1))
         x_test = np.hstack((bias, x_test))
 
@@ -270,49 +278,6 @@ def p_norm_gradient_descent(x_train, y_train, x_validate, y_validate,
     return best_weight
 
 
-def sigmoid_loss_function(x, y, weight):
-
-    number_of_samples, _ = np.shape(x)
-    predict = np.dot(x, weight) - y
-    cost = np.sum(predict ** 2) + lambda_regularizer * np.dot(weight.T, weight)
-    cost = cost / (number_of_samples)
-    return cost, predict
-
-
-def basis_sigmoid_gradient_descent(x_train, y_train, x_validate, y_validate,
-                                   iterations, learning_rate, p):
-
-    number_of_samples, number_of_features = np.shape(x_train)
-    print("Number of train samples: {}, number of train features: {}".format(
-       number_of_samples, number_of_features))
-
-    weight = np.ones(number_of_features)
-    best_validation_loss = math.inf
-    best_weight = None
-    best_train_loss = math.inf
-    for i in range(0, iterations):
-        train_loss, predict = sigmoid_loss_function(x_train, y_train, weight)
-        validation_loss, _ = sigmoid_loss_function(x_validate, y_validate, weight)
-        if i % 100 == 0:
-            print("iteration {}\t| Train_MSE {}\t| Validation_MSE {} ".format(
-                 str(i), str(train_loss), str(validation_loss)))
-
-        gradient = np.dot(predict, x_train) + lambda_regularizer * weight
-        gradient = 2 * learning_rate * gradient / number_of_samples
-        weight = weight - gradient
-        if abs(best_train_loss - train_loss) < error_rate:
-            break
-        if validation_loss > best_validation_loss:
-            break
-        best_weight = weight
-        best_train_loss = train_loss
-        best_validation_loss = validation_loss
-
-    print("final train loss: {} | iteraions: {}".format(train_loss, i))
-
-    return best_weight
-
-
 def test(x_test, weight):
     number_of_samples, number_of_features = np.shape(x_test)
     print("Number of test samples: {}, number of test features: {}".format(
@@ -327,9 +292,20 @@ def test(x_test, weight):
 
 
 def main():
+    global learning_rate, lambda_regularizer
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--p-norm', dest='p', type=int)
+    parser.add_argument('--basis', dest='basis', type=str,
+                        choices=['sigmoid', 'normal'])
+    parser.add_argument('--lambda', dest='lambda_rate', type=float)
+    parser.add_argument('--alpha', dest='learning_rate', type=float)
+    args = parser.parse_args()
 
-    x_train, y_train = read_data("train.csv")
+    learning_rate = args.learning_rate
+    lambda_regularizer = args.lambda_rate
+    p = args.p
 
+    x_train, y_train = read_data("train.csv", basis=args.basis)
     x_validate = x_train[25600:, :]
     x_train = x_train[:25600, :]
 
@@ -337,14 +313,14 @@ def main():
     y_train = y_train[:25600]
 
     number_of_samples, number_of_features = np.shape(x_train)
+    if p is None:
+        weight = gradient_descent(x_train, y_train, x_validate, y_validate,
+                                  iterations, learning_rate)
+    else:
+        weight = p_norm_gradient_descent(x_train, y_train, x_validate, y_validate,
+                                         iterations, learning_rate, p)
 
-    # weight = gradient_descent(x_train, y_train, x_validate, y_validate,
-                              # iterations, learning_rate)
-    p = 6
-    weight = p_norm_gradient_descent(x_train, y_train, x_validate, y_validate,
-                                     iterations, learning_rate, p)
-
-    x_test = read_data_test('test.csv')
+    x_test = read_data_test('test.csv', basis=args.basis)
     test_data = test(x_test, weight)
 
     i = 0
